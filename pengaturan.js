8bf7f13cd8971c36431e24b92872a2411a7d8bbb
moodifypreset <moodifypreset@gmail.com>
2026-06-11 15:15:40 +0700

    Add files via upload

---
+++ pengaturan.js
@@ -0,0 +1,12 @@
+// =========================================================================
+// PENGATURAN APLIKASI CLIENT PORTAL
+// Silakan ubah teks di dalam tanda kutip ("...") dengan data studio Anda.
+// =========================================================================
+
+const CONFIG_APP = {
+    // 1. Nama Studio Foto Anda (Akan tampil di header dan portal klien)
+    namaVendor: "X Studio",
+
+    // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
+    googleApiKey: "ISI_DENGAN_API_KEY_ANDA_DI_SINI"
+};
