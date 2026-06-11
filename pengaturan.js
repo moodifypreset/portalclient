6056d7f915cc298c2c0c39f965ba86cc12ec3610
moodifypreset <moodifypreset@gmail.com>
2026-06-11 15:19:19 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -5,8 +5,8 @@
 
 const CONFIG_APP = {
     // 1. Nama Studio Foto Anda (Akan tampil di header dan portal klien)
-    namaVendor: "X Studio",
+    namaVendor: "Portal Client",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
-    googleApiKey: "ISI_DENGAN_API_KEY_ANDA_DI_SINI"
+    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo"
 };
