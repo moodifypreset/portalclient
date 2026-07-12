1411b6d45972c273cb08edad11abb0b8392ea36d
moodifypreset <moodifypreset@gmail.com>
2026-07-12 17:49:35 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -8,6 +8,6 @@ const CONFIG_APP = {
     namaVendor: "Portal Client",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
-    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo"
+    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
     whatsappAdmin: "6282117102831" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
